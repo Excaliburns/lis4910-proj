3fb5a5315c4fd15fc834380afcd071361fbe9014
Excaliburns <tutsandallthat@gmail.com>
2020-11-02 17:52:46 -0500

    deleted:    client/src/App.css 	modified:   client/src/App.js 	deleted:    client/src/App.test.js 	modified:   client/src/index.js 	deleted:    client/src/reportWebVitals.js 	modified:   client/src/setupTests.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import _ from 'lodash';
+
+class Select extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 1
+    };
+  }
+
+  displayMenu(val) {
+    console.log(val);
+
+    this.props.onHAAA(val);
+    this.setState({value: val});
+  }
+
+  render() {
+    return (
+    <select style={{width: '250px'}} value={this.state.value} onChange={(event) => this.displayMenu(event.target.value)}>
+      {
+        _.range(1, 31).map(x => <option value={x} key={x}>{x}</option>)
+      }
+    </select>
+    )
+  }
+}
+
+async function getMenu(week, day) {
+  let url = new URL('api/menu', window.location.href)
+  let params = {
+                ...week && { week: week },
+                ...day  && { day: day}
+               }
+
+  url.search = new URLSearchParams(params).toString();
+
+  const resp = await fetch(url);
+  const json = await resp.json();
+  return json;
+}
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.style = {
+      display: 'flex', 
+      paddingTop: '5rem',
+      justifyContent: 'center',
+      alignItems: 'middle'
+    }
+
+    this.menu = [];
+
+    this.state = {
+      select: 1
+    }
+  }
+
+  async selectChange(i) {
+    const menu = await getMenu(null, i);
+
+    console.log(menu);
+
+    this.menu = menu;
+
+    this.setState({
+      select: i,
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <div className="App" style={this.style}>
+          <Select 
+          onHAAA = {(i) => this.selectChange(i)}/>
+        </div>
+        <pre id='menu'>{JSON.stringify(this.menu, null, 2)}</pre>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Display />
+  )
 }
 
 export default App;
