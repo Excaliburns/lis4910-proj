e2d1dc69f47c823455538ab53a1095c00b61e878
Excaliburns <tutsandallthat@gmail.com>
2020-11-02 17:33:39 -0500

    new file:   .gitignore 	new file:   client/.gitignore 	new file:   client/README.md 	new file:   client/package-lock.json 	new file:   client/package.json 	new file:   client/public/favicon.ico 	new file:   client/public/index.html 	new file:   client/public/logo192.png 	new file:   client/public/logo512.png 	new file:   client/public/manifest.json 	new file:   client/public/robots.txt 	new file:   client/src/App.css 	new file:   client/src/App.js 	new file:   client/src/App.test.js 	new file:   client/src/index.css 	new file:   client/src/index.js 	new file:   client/src/logo.svg 	new file:   client/src/reportWebVitals.js 	new file:   client/src/setupTests.js 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+
+const cheerio = require('cheerio');
+const axios = require('axios');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log("Process started on port " + PORT)
+})
+
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+  });
+
+app.get('/api/menu', async (req, res) => {
+    console.log(`req came for menu 👐 ! DAY: ${req.query.day}, WEEK: ${req.query.week}`);
+
+
+    const day  = req.query.day;
+    const week = req.query.week
+
+    let foodItems;
+
+    if ( typeof week === 'undefined' ) {
+        foodItems = await getFoodItemsForWeek(undefined, day);
+    }
+    else {
+       foodItems = await getFoodItemsForWeek(week, day);
+    }
+
+    res.json(foodItems);
+})
+
+const pageUrl = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=22663&locationId=40666001&whereami=http://seminoledining.sodexomyway.com/dining-locations/suwanneeroom';
+const datedPageUrl = pageUrl + 'startDate=';
+
+async function getFoodItemsForWeek(week, day) {
+  const weekMenuContent = await axios.get ( week ? (datedPageUrl + (Date.now() * week)) : pageUrl );
+  
+  const $ = cheerio.load(weekMenuContent.data);
+
+  const currentDay = $('#menuid-' + (day ? day : new Date().getDate()) + '-day');
+
+  const menu = []
+
+  currentDay.find('.accordion-block').each( (index, element) => {
+
+    const meal = $(element);
+    const mealTitle = meal.find('span.accordion-copy').first().text();
+    const mealItems = meal.find('.bite-menu-item li .col-xs-9 a').map( (index, element) => { return $(element).text()});
+    menu.push({
+      'mealTitle': mealTitle,
+      'mealItems': mealItems.toArray()
+    });
+  });
+  
+  return menu;
+}
