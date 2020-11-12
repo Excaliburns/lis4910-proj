10bfcaaae041059531eb720e37348508717ca921
Excaliburns <tutsandallthat@gmail.com>
2020-11-12 00:39:42 -0500

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.js 	modified:   client/src/index.css 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,130 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
+import { Typeahead } from 'react-bootstrap-typeahead';
 
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./index.css";
 
-async function getMenu(date) {
+function getMenu(date) {
   let url = new URL('api/menu', window.location.href)
   let params = {
-                ...date && { date: date }
-               }
+    ...date && { date: date }
+  }
 
   url.search = new URLSearchParams(params).toString();
 
-  const resp = await fetch(url, {
+  return fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     },
-    body: JSON.stringify( {date: date} )
-  });
-  const json = await resp.json();
-  return json;
+    body: JSON.stringify({ date: date })
+  })
+    .then(response => {
+      if (response.status === 503) {
+        return { status: 503 }
+      }
+      else {
+        return response.json()
+      }
+    })
+    .then(response => {
+      return response;
+    })
+    .catch(err => { console.log(err); });
 }
 
 class Display extends React.Component {
+  getFoodTypeaheadData() {
+    let url = new URL('api/allFood', window.location.href);
   
+    return fetch(url)
+      .then(response => response.json())
+      .then(response => { return response } );
+  }
+
   // accepts Date
-  async selectChange(date) {
+  selectChange(date) {
     console.log('requesting for: ' + date.toLocaleDateString());
 
     this.setState({
       loading: true,
+      loadingText: 'loading....',
       selectedDate: date,
     });
 
-    getMenu(date).then( (data) => {
-      console.log(data);
-
-      this.menu = data;
-      this.setState({ loading: false });
+    getMenu(date).then(menuResponse => {
+      if (menuResponse.status && menuResponse.status === 503) {
+        this.setState({
+          loading: true,
+          loadingText: 'webserver is still initializing! give us a moment...'
+        })
+      }
+      else {
+        this.menu = menuResponse;
+        this.setState({ loading: false });
+      }
     });
   }
-  
+
   constructor(props) {
     super(props);
-
-    this.style = {
-      display: 'flex', 
-      paddingTop: '5rem',
-      justifyContent: 'center',
-      alignItems: 'middle'
-    }
 
     this.state = {
       selectedDate: new Date(),
       loading: true
     }
 
+    this.getFoodTypeaheadData().then (
+      response =>  {
+        this.setState({ food: response })
+      }
+    )
+
     this.menu = this.selectChange(new Date());
   }
 
-  render () {
+  render() {
     return (
       <div>
-        <div className="App" style={this.style}>
+        <div class="col text-center pt-3 font-weight-bold font-size-large" id="title">Welcome to FSU Dining!</div>
+        <div className="App pt-4 row">
+          <ConditionalTypeahead class="col-6 pl-5" isLoaded={(!this.state.loading)} food={this.state.food} />
           <DatePicker
-           selected={ this.state.selectedDate }
-           onChange={date => this.selectChange(date)}>
+            selected={this.state.selectedDate}
+            onChange={date => this.selectChange(date)}>
           </DatePicker>
         </div>
-        <pre id='menu'>
+        <pre id='menu' className="p-5">
           {
-            this.state.loading ? 'loading...' : JSON.stringify(this.menu, null, 2)
+            this.state.loading ? this.state.loadingText : JSON.stringify(this.menu, null, 2)
           }
-          </pre>
+        </pre>
       </div>
     );
   }
 }
 
+function ConditionalTypeahead(props) {
+  if (props.isLoaded) {
+    return <Typeahead
+      id="foodTypeahead"
+      className={props.class}
+      onChange={(selected) => { console.log(selected) }}
+      options={props.food}
+      labelKey="name"
+      placeholder="Search your favorite foods here...">
+    </Typeahead>
+  }
+  else {
+    return null;
+  }
+}
+
 function App() {
-  return(
+  return (
     <Display />
   )
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 
+const NodeCache = require ('node-cache');
 const cheerio = require('cheerio');
 const axios = require('axios');
 const app = express();
@@ -9,6 +10,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 const PORT = process.env.PORT || 3001;
+
+// Initialize cache
+const myCache = new NodeCache ( { checkperiod: 7200 } );
 
 app.listen(PORT, () => {
   console.log("Process started on port " + PORT)
@@ -30,119 +34,184 @@ app.post('/api/menu', async (req, res) => {
 
   const day = req.query.date;
 
-  let foodItems = await getFoodItemsForWeek(new Date(day));
-
-  res.json(foodItems);
+  if (myCache.has('setupComplete')) {
+    getFoodItemsForWeek(new Date(day)).then (
+      response => res.json(response)
+    );
+  }
+  else {
+    res.status(503).send();
+  }
 })
 
-const pageUrl = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=22663&locationId=40666001&whereami=http://seminoledining.sodexomyway.com/dining-locations/suwanneeroom';
+app.get('/api/allFood', (req, res) => {
+  console.log('Typeahead needs some data...');
+
+  if (myCache.has('allFood')) {
+    res.send(myCache.get('allFood'));
+  }
+  else {
+    res.send({});
+  }
+})
+
+const pageUrl = 'https://menus.sodexomyway.com/BiteMenu/MenuOnly?menuId=22663&locationId=40666001&whereami=http://seminoledining.sodexomyway.com/dining-locations/suwanneeroom';
+
 
 async function getFoodItemsForWeek(date) {
-  const datedPageUrl = pageUrl + '&startDate=' + date.toLocaleDateString();
-  const weekMenuContent = await axios.get(datedPageUrl);
+  const dateString = date.toLocaleDateString();
+  console.log('date: ' + dateString);
 
-  console.log("getting menu for date: " + datedPageUrl)
+  const datedPageUrl = pageUrl + '&startDate=' + dateString;
 
-  const $ = cheerio.load(weekMenuContent.data);
+  if (myCache.has(dateString)) {
+    console.log('date was found in cache. returning from stored data.')
 
-  const currentDay = $('#menuid-' + date.getDay() + '-day');
+    return myCache.get(dateString);
+  }
+  else {
+    const weekMenuContent = await axios.get(datedPageUrl);
 
-  const menu = []
-
-  currentDay.find('.accordion-block').each((index, element) => {
-
-    const meal = $(element);
-
-    // Breakfast, lunch, dinner, etc
-    const mealTitle = meal.find('span.accordion-copy').first().text();
-    const mealItems = [];
-
-    // This unique string of identifiers gets all of the meal items in a single category (breakfast, lunch, dinner)
-    const mealCategory = meal.find('.accordion-panel')
-
-    // In the menu, each category is split between the titles and the actual food in the meal.
-    // Stuff like HOMESTYLE or OMELETE BAR are descriptors for the items. For each of these we can assign it to a child food item
-    // This makes it easier to display later.
-    let currentDescriptor = null;
-
-    mealCategory.children().each((categoryIdx, categoryElement) => {
-      const $element = $(categoryElement);
-
-      // There are only bite-menu-courses and bite-menu-items, all lined up really nicely. 
-      if ($element.hasClass('bite-menu-course')) {
-        const titleText = $element.find('h5').first().text();
-
-        if (titleText.length > 0 && !titleText.includes('-')) {
-          currentDescriptor = titleText.replace(' ', '_');
+    console.log("getting menu for date: " + datedPageUrl)
+  
+    const $ = cheerio.load(weekMenuContent.data);
+  
+    const currentDay = $('#menuid-' + date.getDate() + '-day');
+  
+    const menu = []
+    let allFood = []
+  
+    currentDay.find('.accordion-block').each((index, element) => {
+  
+      const meal = $(element);
+  
+      // Breakfast, lunch, dinner, etc
+      const mealTitle = meal.find('span.accordion-copy').first().text();
+      const mealItems = [];
+  
+      // This unique string of identifiers gets all of the meal items in a single category (breakfast, lunch, dinner)
+      const mealCategory = meal.find('.accordion-panel')
+  
+      // In the menu, each category is split between the titles and the actual food in the meal.
+      // Stuff like HOMESTYLE or OMELETE BAR are descriptors for the items. For each of these we can assign it to a child food item
+      // This makes it easier to display later.
+      let currentDescriptor = null;
+  
+      mealCategory.children().each((categoryIdx, categoryElement) => {
+        const $element = $(categoryElement);
+  
+        // There are only bite-menu-courses and bite-menu-items, all lined up really nicely. 
+        if ($element.hasClass('bite-menu-course')) {
+          const titleText = $element.find('h5').first().text();
+  
+          if (titleText.length > 0 && !titleText.includes('-')) {
+            currentDescriptor = titleText.replace(' ', '_');
+          }
+          else {
+            currentDescriptor = 'HEADER_UNKNOWN_ERR';
+          }
         }
-        else {
-          currentDescriptor = 'HEADER_UNKNOWN_ERR';
-        }
-      }
-
-      // If it's not a course, it's an item!
-      else if ($element.hasClass('bite-menu-item')) {
-        // Find all of <a> tag inside bite-menu-item
-        const mealElements = $element.find('.col-xs-9 a');
-
-        // If it's empty, its a spacer.
-        if (mealElements.length > 0) {
-
-          mealElements.each((itemIdx, itemElement) => {
-            // Get its id so we can get the allergens
-            const idString = $(itemElement).attr('id');
-
-            const idNum = idString.split('-').pop();
-            const descriptorItem = $('#allergens-' + idNum);
-
-            // Extra tag categories:
-            // Allergens [ milk, tree nuts, wheat, gluten, eggs, fish, peanuts, shellfish, soy]
-            // Extra tags [mindful, vegan, vegetarian]
-            const allergens = [];
-            const extras = [];
-
-
-            if ($(descriptorItem).children().length > 0) {
-              $(descriptorItem).children().each((idx, element) => {
-                const altText = $(element).attr('alt');
-
-                // don't add duplicates, for some reason sodexo has them lol
-                // if text contains 'contains\s' then we are an allergen, otherwise descriptor
-                if (altText.includes('contains ')) {
-                  if (!allergens.includes(altText)) {
-                    allergens.push(altText.split('contains ').pop());
+  
+        // If it's not a course, it's an item!
+        else if ($element.hasClass('bite-menu-item')) {
+          // Find all of <a> tag inside bite-menu-item
+          const mealElements = $element.find('.col-xs-9 a');
+  
+          // If it's empty, its a spacer.
+          if (mealElements.length > 0) {
+  
+            mealElements.each((itemIdx, itemElement) => {
+              // Get its id so we can get the allergens
+              const idString = $(itemElement).attr('id');
+  
+              const idNum = idString.split('-').pop();
+              const descriptorItem = $('#allergens-' + idNum);
+  
+              // Extra tag categories:
+              // Allergens [ milk, tree nuts, wheat, gluten, eggs, fish, peanuts, shellfish, soy]
+              // Extra tags [mindful, vegan, vegetarian]
+              const allergens = [];
+              const extras = [];
+  
+  
+              if ($(descriptorItem).children().length > 0) {
+                $(descriptorItem).children().each((idx, element) => {
+                  const altText = $(element).attr('alt');
+  
+                  // don't add duplicates, for some reason sodexo has them lol
+                  // if text contains 'contains\s' then we are an allergen, otherwise descriptor
+                  if (altText.includes('contains ')) {
+                    if (!allergens.includes(altText)) {
+                      allergens.push(altText.split('contains ').pop());
+                    }
                   }
-                }
-                else {
-                  if (!extras.includes(altText)) {
-                    extras.push(altText);
+                  else {
+                    if (!extras.includes(altText)) {
+                      extras.push(altText);
+                    }
                   }
-                }
-              });
-            }
-
-            mealItems.push(
-              {
-                name: $(itemElement).text(),
-                // Really not a fan of this, but when you load the page it will generate random ids for each menu item
-                // These ids are useless between iterations of the page loading, so we just use them to identify what's on the page for whenever it's requested.
-                thisIterUniqueId: idNum,
-                categoryDescriptor: currentDescriptor,
-                descriptors: {
-                  allergens: allergens,
-                  extras: extras
-                }
+                });
               }
-            )
-          });
+  
+              mealItems.push(
+                {
+                  name: $(itemElement).text(),
+                  meal: mealTitle,
+                  date: dateString,
+                  // Really not a fan of this, but when you load the page it will generate random ids for each menu item
+                  // These ids are useless between iterations of the page loading, so we just use them to identify what's on the page for whenever it's requested.
+                  id: idNum,
+                  categoryDescriptor: currentDescriptor,
+                  descriptors: {
+                    allergens: allergens,
+                    extras: extras
+                  }
+                }
+              )
+            });
+          }
         }
+      })
+      if (myCache.has('allFood')) {
+        allFood = [...myCache.get('allFood'), ...mealItems]
       }
-    })
-    menu.push({
-      'mealTitle': mealTitle,
-      'mealItems': mealItems
+      else {
+        allFood = allFood.concat(mealItems)
+      }
+      
+      
+      menu.push({
+        'mealTitle': mealTitle,
+        'mealItems': mealItems
+      });
     });
-  });
 
-  return menu;
+
+    myCache.set ('allFood', allFood);
+    myCache.set (dateString, menu);
+    return menu;
+  }
+}
+
+setupCache().then( result => {
+  myCache.set('setupComplete', true);
+
+  console.log('done initializing! cache is successfully setup.');
+  const lol = myCache.get('allFood');
+
+  console.log(lol.length)
+  const ayy = lol.filter(item => item.name.toLowerCase().includes('meatball'));
+  console.log(ayy)
+});
+
+// When we initialize, load cache with data for today + 2 weeks - 1d
+async function setupCache() {
+  const today = new Date();
+  const aheadDate = new Date();
+  aheadDate.setDate(today.getDate() + 13);
+
+  console.log('initializing cache...');
+  for (let currentDate = today; currentDate <= aheadDate; currentDate.setDate(currentDate.getDate() + 1)) {
+    await getFoodItemsForWeek(currentDate);
+  }
 }
